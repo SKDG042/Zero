23370d104fa4c503648118afedcb699862cbf5c5
SKDG042 <3212580740@qq.com>
2025-10-23 00:50:33 +0800

    feat: 接入简陋的llm chat

---
+++ llm/client.go
@@ -0,0 +1,61 @@
+// Package llm 大模型调用相关
+package llm
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"os"
+	"time"
+
+	openai "github.com/cloudwego/eino-ext/components/model/openai"
+	"github.com/cloudwego/eino/callbacks"
+	"github.com/cloudwego/eino/schema"
+)
+
+// Client LLM调用的客户端
+type Client struct {
+	provider string
+	model    *openai.ChatModel
+	handlers []callbacks.Handler // 有devops包传入链路追踪
+}
+
+// NewOpenaiClient 创建Openai接口格式的客户端
+func NewOpenaiClient(ctx context.Context, provider string, handlers []callbacks.Handler) (*Client, error) {
+	model, err := openai.NewChatModel(ctx, &openai.ChatModelConfig{
+		BaseURL: os.Getenv("OPENAI_BASE_URL"),
+		APIKey:  os.Getenv("OPENAI_API_KEY"),
+		Model:   os.Getenv("OPENAI_MODEL"),
+		Timeout: 30 * time.Second,
+	})
+	if err != nil {
+		if errors.Is(err, context.DeadlineExceeded) {
+			return nil, fmt.Errorf("链接provider超时：%w", err)
+		} else {
+			return nil, fmt.Errorf("创建model失败: %w", err)
+		}
+	}
+
+	Client := &Client{
+		provider: provider,
+		model:    model,
+		handlers: handlers,
+	}
+
+	// 如果创建的Client还有handler, 则自动将其添加到全局
+	if len(Client.handlers) > 0 {
+		callbacks.AppendGlobalHandlers(Client.handlers...)
+	}
+
+	return Client, nil
+}
+
+// Generate 调用client的model生成消息
+func (c *Client) Generate(ctx context.Context, message []*schema.Message) (*schema.Message, error) {
+	response, err := c.model.Generate(ctx, message)
+	if err != nil {
+		return nil, fmt.Errorf("生成消息失败: %w", err)
+	}
+
+	return response, nil
+}
